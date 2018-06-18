12bb672ba2db69a461033f7438ec7a57bbeb36f8
hang <1142469709@qq.com>
2018-06-18 22:37:56 +0800

    提交代码

--- src/print/print.js
+++
@@ -1,6 +0,0 @@
-export default function printMe(){
-    console.log('i get called from print.js!');
-    console.log('i get called from print.js!');
-    cosnole.error('I get called from print.js!');
-}
-alert('print');
